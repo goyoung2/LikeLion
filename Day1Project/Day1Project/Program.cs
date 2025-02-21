4118c70e5c99cd0c0a5fea80eee4fc8a47ee8f73
goyoung2 <42776733+goyoung2@users.noreply.github.com>
2025-02-21 09:57:12 +0900

    Study
    Day1 멋사 첫번재 git 커밋 Test

---
+++ Day1Project/Day1Project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //깃허브 업로드 확인용
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
